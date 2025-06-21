df1590d2e25fd2362cd8f55637de84eae253ea29
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-21 11:13:23 -0300

    Create exerc_04.py

---
+++ exerc_04.py
@@ -0,0 +1,5 @@
+velocidade = float(input('Qual é a velocidade atual do carro ? :'))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido da via que é 80Km/h')
+print('Tenha um bom dia! "Dirija com segurança! " ')
+
