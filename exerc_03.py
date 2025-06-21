7c543f7b6460134ebc354f3bb36aa9700c33f068
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-21 10:39:59 -0300

    Update exerc_03.py

---
+++ exerc_03.py
@@ -0,0 +1,8 @@
+from random import randint
+computador = randint(0, 5) #Faz o computador "PENSAR"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=- * 20')
+jogador = int(input('Em que número eu pensei? ')) # jogador tenta adivinhar
+
+
