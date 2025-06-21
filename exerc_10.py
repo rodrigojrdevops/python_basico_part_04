eea90aa5c065c502568a1625305210ad002e9f11
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-21 13:24:48 -0300

    Update exerc_10.py

--- exerc_10.py
+++ exerc_10.py
@@ -5,7 +5,7 @@ r1 = float(input('Digite o primeiro segmento: '))
 r2 = float(input('Digite o segundo segmento: '))
 r3 = float(input('Digite o terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 <r1 + r2:
-    print('Os segmentos acima formam um triãngulo: ')
+    print('Os segmentos acima "PODEM FORMAR UM TRIÂNGULO": ')
 else:
-    print('Os valores acima não formam um triângulo: ')
-    print('-=-' *20)
+    print('Os segmentos acima "NÃO PODEM FORMAR UM TRIÂNGULO" : ')
+
