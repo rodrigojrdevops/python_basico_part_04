2aca80f5c5c6ce650c2307007c20f1ea89df5164
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-21 11:17:52 -0300

    Update exerc_04.py

--- exerc_04.py
+++ exerc_04.py
@@ -1,5 +1,7 @@
 velocidade = float(input('Qual é a velocidade atual do carro ? :'))
 if velocidade > 80:
     print('MULTADO! Você excedeu o limite permitido da via que é 80Km/h')
+    multa = (velocidade - 80) *7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
 print('Tenha um bom dia! "Dirija com segurança! " ')
 
